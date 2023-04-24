41dccc785b6a6638ded67539db660626995eaba2
Darcy <darcylgeyer@gmail.com>
2023-04-24 23:01:11 +0930

    Implemented ability to disable raw mode

--- kilo.c
+++ kilo.c
@@ -1,15 +1,26 @@
+#include <stdlib.h> 
 #include <termios.h> // For enabling raw mode
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
+    
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode); // disabled Raw Mode when program exits
+
+    struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 
 int main() {
+    enableRawMode();
 
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'); // Read one byte from std input at a time into c, until there are none left to read in. (read() returns no. of bytes read in)
